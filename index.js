5400ea423b91417c16a1a3766f222d627a37504d
Riverbraid <f7pbmpwbfn@privaterelay.appleid.com>
2026-02-11 13:59:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-import crypto from 'node:crypto';
-const hash = crypto.createHash('sha256').update('Source').digest('hex');
-console.log(JSON.stringify({ status: "STATIONARY", cell: "crypto-gold", anchor: hash }));
+export const RB_PETAL_ID = "Riverbraid-Crypto-Gold"; console.log(JSON.stringify({ status: "STATIONARY", petal: RB_PETAL_ID }));
